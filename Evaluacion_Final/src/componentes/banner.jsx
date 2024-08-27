e66095bd3967f637fcdcea5129f2d0ef986b942d
Daniela1701 <i202409920@cibertec.edu.pe>
2024-08-27 19:46:32 +0000

    baner listo

--- Evaluacion_Final/src/componentes/banner.jsx
+++ Evaluacion_Final/src/componentes/banner.jsx
@@ -1,30 +1,37 @@
+import React, { useState, useEffect } from 'react'; 
+import mueble1 from '../imagenes/mueble1.jpg';
+import mueble2 from '../imagenes/mueble2.jpg';
 
-import mueble1 from '../imagenes/mueble1.jpg'
-import mueble2 from '../imagenes/mueble2.jpg'
+function Banner() {
 
-function Banner()
-{
-return(
-  <>
-  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={mueble1} className='d-block w-100' />
-    </div>
-    <div class="carousel-item">
-      <img src={mueble2} className="d-block w-100" />
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-  </>
-  )
+  const [indiceImg, nuevaImg] = useState(0);
+
+  // Array de imágenes
+  const imagen = [mueble1, mueble2];
+
+  useEffect(() => {
+    // Función para rotar la imagen
+    const rotarImagen = () => {
+      nuevaImg((indicePrevio) => (indicePrevio + 1) % imagen.length);
+    };
+
+    // Iniciar la rotación cada segundo
+    const intervaloId = setInterval(rotarImagen, 3000);
+
+    // Agregamos console.log para depuración
+    console.log(`Imagen actual: ${imagen[indiceImg]}`);
+
+    // Limpiar el intervalo cuando el componente se desmonte
+    return () => clearInterval(intervaloId);
+  }, [indiceImg, imagen]); // Aseguramos que se vuelva a ejecutar el efecto cuando cambie el índice o las imágenes
+
+  return (
+        <div id="Banner">
+            <section>
+            <img id="imagenBanner" src={imagen[indiceImg]} alt={`Banner ${indiceImg}`} />
+            </section>
+        </div>
+
+  );
 }
-export default Banner
+export default Banner;
