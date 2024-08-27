bae16ca1507291623e90e117c7e5995da42fb95e
Daniela1701 <i202409920@cibertec.edu.pe>
2024-08-27 18:53:10 +0000

    logos

--- Evaluacion_Final/src/componentes/cabecera.jsx
+++ Evaluacion_Final/src/componentes/cabecera.jsx
@@ -1,13 +1,22 @@
-
-import '../componentes/muebles.css'
-
+import logo from '../imagenes/logo.png'
+import facebook from '../imagenes/facebook.png'
+import instagram from '../imagenes/youtube.png'
 function Cabecera() 
 {
 
   return (
     <>
-    <div>
-      <h1>MueblesFAST</h1>
+    <div className="cabecera">
+        <img src={logo} />
+        <h1>MueblesFAST</h1>
+
+        <div className='redes'>
+            <img src={facebook}  />
+            <img src={instagram} />
+            <img src={youtube} />
+
+
+        </div>
     </div>
       
     </>
