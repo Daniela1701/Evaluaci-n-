27229e7ba7693d47dc37ed3beb60d94b2faf0c70
Daniela1701 <i202409920@cibertec.edu.pe>
2024-08-27 18:54:41 +0000

    cabecera lista

--- Evaluacion_Final/src/componentes/cabecera.jsx
+++ Evaluacion_Final/src/componentes/cabecera.jsx
@@ -1,6 +1,8 @@
 import logo from '../imagenes/logo.png'
 import facebook from '../imagenes/facebook.png'
-import instagram from '../imagenes/youtube.png'
+import instagram from '../imagenes/instagram.png'
+import youtube from '../imagenes/youtube.png'
+
 function Cabecera() 
 {
 
@@ -9,13 +11,10 @@ function Cabecera()
     <div className="cabecera">
         <img src={logo} />
         <h1>MueblesFAST</h1>
-
         <div className='redes'>
             <img src={facebook}  />
             <img src={instagram} />
             <img src={youtube} />
-
-
         </div>
     </div>
       
